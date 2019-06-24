f2f8e9b3f95b9ad312282ba4a1a0c6c03c6b9566
Shail <shailpatel67@gmail.com>
2019-06-24 01:53:05 -0400

    updated qr generator

--- qr/generateQr.py
+++ qr/generateQr.py
@@ -5,6 +5,7 @@ from sys import argv
 import qrcode
 import os
 import sys
+import urllib
 from PIL import Image
 
 data = input("enter a string to encode\n")
@@ -14,6 +15,6 @@ qr = qrcode.QRCode(
     border = 4,
 )
 qr.add_data(data)
+data = urllib.parse.quote_plus(data)
 img = qr.make_image()
 img.save('output/' + data + '.png') #can also be .bmp, .jpeg
-sys.exit
